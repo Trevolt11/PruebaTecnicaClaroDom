32dee4d70e539828289e7f059c6c50cd7d1190fb
robert <rrobertccaraballo@gmail.com>
2023-10-16 20:13:05 -0400

    WebApi_ClaroDom

--- WebApi/Model/BookModel.cs
+++ WebApi/Model/BookModel.cs
@@ -3,9 +3,7 @@
     public class Book
     {
         public int Id { get; set; }
-
         public string Title { get; set; }
-
         public string Description { get; set; }
         public int PageCount { get; set; }
         public string Excerpt { get; set; }
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -4,6 +4,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var MyAllowAnyOrigin = "_myAllowAnyOrigin";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowAnyOrigin,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin(); // Permite cualquier origen
+                          policy.AllowAnyMethod();
+                          policy.AllowAnyHeader();
+                      });
+});
+
+
+
 builder.Services.AddControllers();
 builder.Services.AddHttpClient();
 builder.Services.AddScoped<IBookService, BookService>();
@@ -19,6 +34,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowAnyOrigin);
+
 
 app.UseHttpsRedirection();
 
